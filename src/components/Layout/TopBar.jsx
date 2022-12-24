3af3473eac0ce88eb3a3e9c14bee0a12dc819f3c
harshsinghdev <harshsingh.bharat@gmail.com>
2022-12-24 20:36:40 +0530

    Updated layout

--- src/components/Layout/TopBar.jsx
+++
@@ -1,7 +0,0 @@
-export default function TopBar({ children }) {
-    return (
-        <div className="top-bar">
-            {children}
-        </div>
-    )
-}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import ContainerGrid from "../components/Layout/ContainerGrid.jsx";
 import Main from "../components/Layout/Main.jsx";
 import SideBar from "../components/Layout/SideBar.jsx";
-import TopBar from "../components/Layout/TopBar.jsx";
+import Header from "../components/Layout/Header.jsx";
 
 export default function Home() {
     return (
         <ContainerGrid>
-            <TopBar>Top bar</TopBar>
+            <Header>
+                <div className="top-bar">
+                    Top bar
+                </div>
+            </Header>
             <SideBar>Side bar</SideBar>
             <Main>Main</Main>
         </ContainerGrid>
